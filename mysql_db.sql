8d2ca52428a4d624d7795b494e901d4096598c1a
SAMRUDDHI KHAIRNAR <samruddhi2929@gmail.com>
2017-01-20 04:50:04 -1200

    final db

--- mysql_db.sql
+++ mysql_db.sql
@@ -1,6 +1,6 @@
 use db;
 
-create table login(
+create table user_data(
     user_id int(11) primary key,
     username varchar(20),
     password varchar(20)
@@ -11,22 +11,12 @@ create table user_saved_stock(
     stocksymbol varchar(10),
     stockprice decimal(10,2),
     stockquantity int(5),
-    FOREIGN KEY(user_id) REFERENCES login(user_id)
+    FOREIGN KEY(user_id) REFERENCES login(user_id),
+    FOREIGN KEY(stocksymbol) REFERENCES stock_for_cap(stocksymbol),
     );
     
- create table stock(
-    stocksym varchar(10),
+create table stock_for_Cap(
+    stocksymbol varchar(10) primary key,
     marketcap varchar(20)
     );
-        
-    insert into login values(1,"mohit","citi");
-    insert into login values(2,"munir","citi");
-    insert into login values(3,"tejal","citi");
-    insert into login values(4,"sneha","citi");
-    insert into login values(5,"apoorva","citi");
-    insert into login values(6,"z","citi");
-    
-    select * from login;
-    
-    select * from user_saved_stock;
-    
+          
